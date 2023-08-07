5a72786ad5a2c682446e87e1f5a4564e2cd9ef8c
armel.vidali <vidali.armel@gmail.com>
2023-08-07 16:18:34 +0200

    modified shape selection on click

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const turf = require("@turf/turf");
-var map = L.map("map").setView([1.603354, 1.888334], 6);
+const Swal = require("sweetalert2");
+var map = L.map("map", { zoomControl: false }).setView(
+  [46.603354, 1.888334],
+  6
+);
 map.doubleClickZoom.disable();
 var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -8,159 +12,197 @@ var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 var ids = 0;
-let ctrlKeyPressed = false;
+var ctrlKeyPressed = false;
+var shiftKeyPressed = false;
 var clicked_path;
-
-let bigger_btn = document.createElement("button");
-let smaller_btn = document.createElement("button");
-bigger_btn.textContent = "bigger";
-bigger_btn.id = "bigger";
-smaller_btn.textContent = "smaller";
-smaller_btn.id = "smaller";
-smaller_btn.textContent = "smaller";
-let ctrl_div = document.querySelector(
-  '[class*="leaflet-control-zoom leaflet-bar leaflet-control"]'
-);
-ctrl_div.appendChild(bigger_btn);
-ctrl_div.appendChild(smaller_btn);
-
-let download_button = `<button class="btn" id="download_btn" role="button">Download</button>`;
-ctrl_div.innerHTML += download_button;
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Control" || event.key === "Meta") {
     ctrlKeyPressed = true;
+  } else if (event.key === "Shift") {
+    shiftKeyPressed = true;
   }
 });
 
 document.addEventListener("keyup", function (event) {
   if (event.key === "Control" || event.key === "Meta") {
     ctrlKeyPressed = false;
+  } else if (event.key === "Shift") {
+    shiftKeyPressed = true;
   }
 });
 
-async function processData() {
-  try {
-    const fetchedData = await fetch("france.geojson");
-    if (!fetchedData.ok) {
-      throw new Error("Failed to fetch GeoJSON data");
-    }
+let fileInput = document.getElementById("file_input");
+fileInput.addEventListener("click", () => {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = [".json", ".geojson"]; // Specify the accepted file type(s) here
 
-    const data = await fetchedData.json();
-    var i = 0;
+  fileInput.addEventListener("change", (event) => {
+    const selectedFile = event.target.files[0];
 
-    let geojsonLayer = L.geoJSON(data, {
-      draggable: false,
-      test: "test",
-      style: {
-        weight: 0.2,
-        fillOpacity: 0.9,
-        color: "black",
-        fillColor: "white",
-      },
-    }).addTo(map);
+    try {
+      const reader = new FileReader();
 
-    geojsonLayer.eachLayer(function (path) {
-      path.feature.id = i++;
-      path._path.classList.add("original_path");
-    });
+      reader.onload = (e) => {
+        const contents = e.target.result;
+        console.log(e);
+        processData(JSON.parse(contents));
+        Swal.fire(
+          "Layer loaded !",
+          "Click on <kbd class = kdb_elem >Ctrl</kbd> + <kbd class = kdb_elem >Left-Click</kbd> to duplicated a shape. <br/><br/> Use <kbd class = kdb_elem >Left-Click</kbd> to move it",
+          "success"
+        );
+        let layer_name = document.createElement("li");
+        layer_name.textContent += "- " + event.target.files[0].name;
+        layer_name.className = "layer_name";
 
-    function bind_drag(feature, collection = false) {
-      feature.eachLayer(function (path) {
-        path.on("dragend", function (event) {
-          this.setStyle({ fillColor: "white" });
-          let flag = false;
-          var bounds = path._bounds;
+        let menu_list = document.getElementById("menu_ul");
 
-          for (let classname of path._path.classList) {
-            if (classname.includes("dragged_path")) {
-              flag = true;
-            }
-          }
-          if (flag == false) {
-            path._path.classList.add(`dragged_path_${path.feature.id}`);
-          }
-          try {
-            let bouding_rectangle = document.querySelectorAll(
-              `[class*="dragged_rectangle_${path.feature.id}"]`
-            );
-            bouding_rectangle[0].remove();
-          } catch {}
-          let bounds_coords = [
-            [bounds._northEast.lat, bounds._northEast.lng],
-            [bounds._southWest.lat, bounds._southWest.lng],
-          ];
-          let bouding_rectangle = L.rectangle(bounds, {
-            color: "black",
-            weight: 1,
-            fill: false,
-          }).addTo(map);
-          bouding_rectangle._path.classList.add(
-            `dragged_rectangle_${path.feature.id}`
-          );
-        });
-      });
-    }
+        menu_list.insertBefore(layer_name, menu_list.children[1]);
+      };
 
-    function redraw(path, scale) {
-      try {
-        var path_id = path._layers[Object.keys(path._layers)].feature.id;
-      } catch {}
-      try {
-        var path_id = path.feature.id;
-      } catch {}
-      document
-        .querySelectorAll(`[class*="dragged_rectangle_${path_id}"]`)[0]
-        .remove();
-      path.remove();
-
-      if (scale == "in") {
-        var rescaled_path = L.geoJSON(
-          turf.transformScale(path.toGeoJSON(), 1.1),
-          {
-            draggable: true,
-            style: {
-              weight: 0.2,
-              fillOpacity: 0.9,
-              color: "black",
-              fillColor: "white",
-            },
-          }
-        ).addTo(map);
-      } else {
-        var rescaled_path = L.geoJSON(
-          turf.transformScale(path.toGeoJSON(), 0.9),
-          {
-            draggable: true,
-            style: {
-              weight: 0.2,
-              fillOpacity: 0.9,
-              color: "black",
-              fillColor: "white",
-            },
-          }
-        ).addTo(map);
-      }
-      rescaled_path._layers[
-        Object.keys(rescaled_path._layers)
-      ]._path.classList.add(
-        "dragged_path_" +
-          rescaled_path._layers[Object.keys(rescaled_path._layers)].feature.id
+      reader.readAsText(selectedFile);
+    } catch {
+      Swal.fire(
+        "Invalid data !",
+        "Please load data in a correct JSON or GeoJSON format",
+        "error"
       );
-      let bounds =
-        rescaled_path._layers[Object.keys(rescaled_path._layers)]._bounds;
+    }
+  });
+  fileInput.click();
+});
 
-      let bounding_rectangle = L.rectangle(bounds, {
-        color: "black",
-        weight: 1,
-        fill: false,
-      }).addTo(map);
-      bounding_rectangle._path.classList.add("dragged_rectangle_" + path_id);
-      bind_drag(rescaled_path, false);
-      clicked_path = rescaled_path;
-      rescaled_path.addEventListener("click", function () {
-        clicked_path = rescaled_path;
+function delete_path(path) {
+  map.on("contextmenu", function (event) {
+    if (event.which === 3) {
+      console.log("right");
+      /* Right mouse button was clicked! */
+    }
+  });
+}
+
+function processData(data) {
+  var i = 0;
+
+  let geojsonLayer = L.geoJSON(data, {
+    draggable: false,
+    test: "test",
+    style: {
+      weight: 0.2,
+      fillOpacity: 0.9,
+      color: "black",
+      fillColor: "white",
+    },
+  }).addTo(map);
+
+  delete_path(geojsonLayer);
+
+  geojsonLayer.eachLayer(function (path) {
+    path.feature.id = i++;
+    path._path.classList.add("original_path");
+  });
+
+  function bind_drag(feature, collection = false) {
+    feature.eachLayer(function (path) {
+      path.on("dragend", function (event) {
+        this.setStyle({ fillColor: "white" });
+        let flag = false;
+        var bounds = path._bounds;
+
+        for (let classname of path._path.classList) {
+          if (classname.includes("dragged_path")) {
+            flag = true;
+          }
+        }
+        if (flag == false) {
+          path._path.classList.add(`dragged_path_${path.feature.id}`);
+        }
+        try {
+          let bouding_rectangle = document.querySelectorAll(
+            `[class*="dragged_rectangle_${path.feature.id}"]`
+          );
+          bouding_rectangle[0].remove();
+        } catch {}
+        let bounds_coords = [
+          [bounds._northEast.lat, bounds._northEast.lng],
+          [bounds._southWest.lat, bounds._southWest.lng],
+        ];
+        let bouding_rectangle = L.rectangle(bounds, {
+          color: "black",
+          weight: 1,
+          fill: false,
+        }).addTo(map);
+        bouding_rectangle._path.classList.add(
+          `dragged_rectangle_${path.feature.id}`
+        );
       });
+    });
+  }
+
+  function redraw(path, scale) {
+    try {
+      var path_id = path._layers[Object.keys(path._layers)].feature.id;
+    } catch {}
+    try {
+      var path_id = path.feature.id;
+    } catch {}
+    document
+      .querySelectorAll(`[class*="dragged_rectangle_${path_id}"]`)[0]
+      .remove();
+    path.remove();
+
+    if (scale == "in") {
+      var rescaled_path = L.geoJSON(
+        turf.transformScale(path.toGeoJSON(), 1.1),
+        {
+          draggable: true,
+          style: {
+            weight: 0.2,
+            fillOpacity: 0.9,
+            color: "black",
+            fillColor: "white",
+          },
+        }
+      ).addTo(map);
+    } else {
+      var rescaled_path = L.geoJSON(
+        turf.transformScale(path.toGeoJSON(), 0.9),
+        {
+          draggable: true,
+          style: {
+            weight: 0.2,
+            fillOpacity: 0.9,
+            color: "black",
+            fillColor: "white",
+          },
+        }
+      ).addTo(map);
+    }
+    rescaled_path._layers[
+      Object.keys(rescaled_path._layers)
+    ]._path.classList.add(
+      "dragged_path_" +
+        rescaled_path._layers[Object.keys(rescaled_path._layers)].feature.id
+    );
+    let bounds =
+      rescaled_path._layers[Object.keys(rescaled_path._layers)]._bounds;
+
+    let bounding_rectangle = L.rectangle(bounds, {
+      color: "black",
+      weight: 1,
+      fill: false,
+    }).addTo(map);
+    bounding_rectangle._path.classList.add("dragged_rectangle_" + path_id);
+    bind_drag(rescaled_path, false);
+    rescaled_path._layers[
+      Object.keys(rescaled_path._layers)
+    ]._path.classList.add("clicked");
+    clicked_path = rescaled_path;
+
+    rescaled_path.addEventListener("click", function () {
+      clicked_path = rescaled_path;
       if (
         Array.from(
           rescaled_path._layers[Object.keys(rescaled_path._layers)]._path
@@ -174,7 +216,6 @@ async function processData() {
         map.eachLayer(function (path) {
           if (path instanceof L.Path) {
             if (Array.from(path._path.classList).includes("clicked")) {
-              console.log(path);
               path._path.classList.remove("clicked");
             }
           }
@@ -183,68 +224,76 @@ async function processData() {
           Object.keys(rescaled_path._layers)
         ]._path.classList.add("clicked");
       }
-    }
-
-    geojsonLayer.eachLayer(function (path) {
-      path.on("click", function (event) {
-        if (ctrlKeyPressed) {
-          var new_path = L.geoJSON(this.toGeoJSON(), {
-            draggable: true,
-            style: {
-              weight: 0.2,
-              fillOpacity: 0.9,
-              color: "black",
-              fillColor: "orange",
-            },
-          }).addTo(map);
-          bind_drag(new_path, false);
-          clicked_path = new_path;
-
-          new_path.addEventListener("click", function () {
-            clicked_path = new_path;
-            if (
-              Array.from(
-                new_path._layers[Object.keys(new_path._layers)]._path.classList
-              ).includes("clicked")
-            ) {
-              new_path._layers[
-                Object.keys(new_path._layers)
-              ]._path.classList.remove("clicked");
-            } else {
-              map.eachLayer(function (path) {
-                if (path instanceof L.Path) {
-                  if (Array.from(path._path.classList).includes("clicked")) {
-                    console.log(path);
-                    path._path.classList.remove("clicked");
-                  }
-                }
-              });
-              new_path._layers[
-                Object.keys(new_path._layers)
-              ]._path.classList.add("clicked");
-            }
-          });
-        }
-      });
     });
-    document.getElementById("bigger").addEventListener("click", function () {
-      redraw(clicked_path, "in");
-    });
-    document.getElementById("smaller").addEventListener("click", function () {
-      redraw(clicked_path, "out");
-    });
-  } catch (error) {
-    console.error("An error occurred:", error);
   }
+
+  geojsonLayer.eachLayer(function (path) {
+    path.on("click", function (event) {
+      if (ctrlKeyPressed) {
+        var new_path = L.geoJSON(this.toGeoJSON(), {
+          draggable: true,
+          style: {
+            weight: 0.2,
+            fillOpacity: 0.9,
+            color: "black",
+            fillColor: "orange",
+          },
+        }).addTo(map);
+        bind_drag(new_path, false);
+        clicked_path = new_path;
+
+        new_path.addEventListener("click", function () {
+          clicked_path = new_path;
+          if (
+            Array.from(
+              new_path._layers[Object.keys(new_path._layers)]._path.classList
+            ).includes("clicked")
+          ) {
+            new_path._layers[
+              Object.keys(new_path._layers)
+            ]._path.classList.remove("clicked");
+          } else {
+            map.eachLayer(function (path) {
+              if (path instanceof L.Path) {
+                if (Array.from(path._path.classList).includes("clicked")) {
+                  path._path.classList.remove("clicked");
+                }
+              }
+            });
+            new_path._layers[Object.keys(new_path._layers)]._path.classList.add(
+              "clicked"
+            );
+          }
+        });
+      }
+    });
+  });
+  document.getElementById("bigger").addEventListener("click", function () {
+    redraw(clicked_path, "in");
+  });
+  document.getElementById("smaller").addEventListener("click", function () {
+    console.log(map.getBounds());
+    redraw(clicked_path, "out");
+  });
 }
 
-processData();
-var bounds = [
-  [43.02778865693062, 9.603171584462599],
-  [41.37865421129973, 8.584047853251082],
-];
-// zoom the map to the rectangle bounds
-map.fitBounds(bounds);
+fileInput.addEventListener("change", (event) => {
+  const selectedFile = event.target.files[0];
+
+  if (selectedFile) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const contents = e.target.result;
+      fileContents.textContent = contents;
+    };
+
+    reader.readAsText(selectedFile);
+  } else {
+    fileContents.textContent = "No file selected.";
+  }
+});
+
 let download_btn = document.getElementById("download_btn");
 
 download_btn.addEventListener("click", function () {
